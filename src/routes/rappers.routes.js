c7d82a2d5093fcab12d145692eb02df341413d1b
anna leme <162629023+annabialeme@users.noreply.github.com>
2024-10-17 20:38:17 -0300

    feat: Rota para buscar um rapper pelo id

--- src/routes/rappers.routes.js
+++ src/routes/rappers.routes.js
@@ -1,3 +1,44 @@
 import { Router } from "express";
 
 const rappersRoutes = Router();
+
+let rappers = [
+    {
+        id: Number(Math.floor(Math.random() * 999999) +1),
+        nome: "Beyoncé",
+        idade: 43,
+        descriçãoFisica : "morena, cabelo loiro, cantora",
+        envolvimentoNoCrime: true,
+        
+    },
+    {
+        id: Number(Math.floor(Math.random() * 999999) +1),
+        nome: "Jay-Z",
+        idade: 54,
+        descriçãoFisica : "negro, cabelo preto, casado",
+        envolvimentoNoCrime: true,
+    },
+    {
+        id: Number(Math.floor(Math.random() * 999999) +1),
+        nome: "Justin Bieber",
+        idade: 30,
+        descriçãoFisica : "branco, cabelo castanho claro, cantor",
+        envolvimentoNoCrime: false,
+    }, 
+    {
+        id: Number(Math.floor(Math.random() * 999999) +1),
+        nome: "Selena Gomez",
+        idade: 32,
+        descriçãoFisica : "branca, cabelo castanho escuro, cantora",
+        envolvimentoNoCrime: false,
+    }
+];
+
+// Rota para listar todos os rappers 
+rappersRoutes.get("/", (req, res) => {
+    return res.status(200).json(rappers);
+});
+
+// Rota para buscar um rappers pelo id
+rappersRoutes.get("/:id", (req, res) => {
+    const { id } = req.params;
